d4c422859b074dbbc2be3593afbc657e16dd2cab
Adam Wajahat <adamwonline0@yahoo.com>
2023-02-25 14:29:47 -0600

    Animation of main logo in center (kind of jank)

---
+++ src/components/Layout/index.js
@@ -0,0 +1,12 @@
+import './index.scss'
+import MiddleLogo from '../MiddleLogo'
+
+const Layout = () => {
+    return (
+        <>
+            <MiddleLogo />
+        </>
+    )
+}
+
+export default Layout
