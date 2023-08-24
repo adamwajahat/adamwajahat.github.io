375dc11cc719ea0bfbb73aeb16fd4fb42ebd318d
Adam Wajahat <adamwonline0@yahoo.com>
2023-08-23 20:06:19 -0500

    Blank slate

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
-import './App.scss';
-import { Routes } from 'react-router-dom'
-import { Route } from 'react-router-dom'
-import Home from './components/Home'
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
-    <>
-    <Routes>
-      <Route path="/" element={<Home />}></Route>
-    </Routes>
-    </>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
