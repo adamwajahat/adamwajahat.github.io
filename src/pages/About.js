4cd404f2ed3dd8751f6252ba9e857f41baae06b4
Adam Wajahat <adamwonline0@yahoo.com>
2023-08-29 20:00:54 -0500

    inital set up of pages (only desktop)

---
+++ src/pages/About.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import '../styles/About.css'
+
+function About() {
+  return (
+    <div className="about">
+      <div className="text">
+          <span className="italicFont">Bismillahir Rahmanir Rahmeen</span>,
+          <br></br>
+          <br></br>
+          <span className="italicFont">Adam Wajahat</span>, a muslim contemporary artist, computer engineer, and designer, based in Dallas, Texas.
+          <br></br> 
+          <br></br>
+          <a href="https://instagram.com/adamwajahat">https://instagram.com/adamwajahat</a>
+      </div>
+    </div>
+  )
+}
+
+export default About
